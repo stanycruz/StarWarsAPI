65e6f00d00874cc0d6f8dcc88fa989f7cc221a59
Stanislaw Cruz <stanycruz@outlook.com>
2019-08-29 00:21:32 -0300

    Implementação da consulta via API.

---
+++ Helpers/ProtocolsHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+
+namespace StarWarsAPI.Helpers
+{
+    internal class ProtocolsHelper
+    {
+        /// <summary>
+        /// Utilizado para URL's que possuem nível de segurança (https).
+        /// </summary>
+        public static void SecurityProtocol()
+        {
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls
+                | SecurityProtocolType.Tls11
+                | SecurityProtocolType.Tls12
+#pragma warning disable CS0618 // Type or member is obsolete
+                | SecurityProtocolType.Ssl3;
+#pragma warning restore CS0618 // Type or member is obsolete
+        }
+    }
+}
---
+++ Helpers/RequestHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+
+namespace StarWarsAPI.Helpers
+{
+    internal class RequestHelper
+    {
+        /// <summary>
+        /// Evita o erro de cabeçalhos com caracteres CR e LF.
+        /// </summary>
+        /// <param name="request"></param>
+        public static void ContainsVary(HttpWebRequest request)
+        {
+            if (request.Headers.AllKeys.Contains("Vary"))
+                request.Headers["Vary"] = "Accept";
+            else
+                request.Headers.Add("Vary", "Accept");
+        }
+
+        /// <summary>
+        /// Verifica de existe autenticação via proxy.
+        /// </summary>
+        /// <param name="request"></param>
+        public static void ProxyAuthentication(HttpWebRequest request)
+        {
+            IWebProxy proxy = request.Proxy;
+
+            if (proxy != null)
+            {
+                WebProxy myProxy = new WebProxy();
+                string uri = proxy.GetProxy(request.RequestUri).ToString();
+                Uri newUri = new Uri(uri);
+                myProxy.Address = newUri;
+                myProxy.Credentials = new NetworkCredential("proxyLogin", "proxyPass");
+                request.Proxy = myProxy;
+                request.ProtocolVersion = HttpVersion.Version10;
+            }
+        }
+    }
+}
